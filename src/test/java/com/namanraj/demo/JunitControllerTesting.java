24dfb6fb5c34ebe79248bf65ae12dc6390a0cb1c
Rachna Jain <Rachna.Jain@iiitb.org>
2020-05-26 15:58:09 +0530

    tested with JUnit

--- src/test/java/com/namanraj/demo/JunitControllerTesting.java
+++ src/test/java/com/namanraj/demo/JunitControllerTesting.java
@@ -1,12 +1,14 @@
 package com.namanraj.demo;
 
+import com.namanraj.demo.controller.PageController;
+import com.namanraj.demo.controller.StudentController;
+import com.namanraj.demo.controller.loginController;
+import com.namanraj.demo.controller.complaintController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.namanraj.demo.controller.complaintController;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -17,9 +19,31 @@ public class JunitControllerTesting {
 	complaintController complaintController;
 	
 	@Test
-	public void testUsertableControllerInitialization()	{
+	public void testComplaintControllerInitialization()	{
 		assertThat(complaintController).isNotNull();
 	}
-	
-	
+
+	@Autowired
+	loginController loginController;
+
+	@Test
+	public void testloginControllerInitialization()	{
+		assertThat(loginController).isNotNull();
+	}
+
+	@Autowired
+	PageController pageController;
+
+	@Test
+	public void testpageControllerInitialization()	{
+		assertThat(pageController).isNotNull();
+	}
+
+	@Autowired
+	StudentController studentController;
+
+	@Test
+	public void teststudentControllerInitialization()	{
+		assertThat(studentController).isNotNull();
+	}
 }
