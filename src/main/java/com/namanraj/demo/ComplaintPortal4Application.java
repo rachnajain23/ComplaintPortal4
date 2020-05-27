53011412eeaaefff56a7929c6f557b7bca275cd4
Ashutosh Singh <ashumrsingh@gmail.com>
2020-05-27 22:54:33 +0530

    Update ComplaintPortal4Application.java

--- src/main/java/com/namanraj/demo/ComplaintPortal4Application.java
+++ src/main/java/com/namanraj/demo/ComplaintPortal4Application.java
@@ -2,12 +2,13 @@ package com.namanraj.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ComplaintPortal4Application {
+public class ComplaintPortal4Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComplaintPortal4Application.class, args);
 	}
-
+ 
 }
